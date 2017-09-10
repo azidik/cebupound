2b662af845f3409906c1c5990402e2e9d3c3b003
Shamesse Ann Delda <Shamesse Ann Delda>
2017-09-10 23:50:09 +0800

    pending

--- resources/views/dashboard/admin/impound/list.blade.php
+++ resources/views/dashboard/admin/impound/list.blade.php
@@ -44,7 +44,9 @@
                                 <td>{{ $impound->pet->color }}</td>
                                 <td>{{ $impound->pet->type->name }}</td>
                                 <td>{{ $impound->pet->user->first_name }}</td>
-                                @if($impound->is_accepted == 1)
+                                @if($impound->is_accepted == 0) 
+                                <td><button class="btn btn-warning btn-xs" disabled="true">Pending</button><td>
+                                @elseif($impound->is_accepted == 1)
                                     <td><small class="label label-primary"><i class="fa fa-thumbs-o-up"></i> Accepted</small>  </td>
                                 @elseif($impound->is_accepted == 2)
                                     <td><small class="label label-danger"><i class="fa fa-thumbs-o-down"></i> Declined</small></td>
