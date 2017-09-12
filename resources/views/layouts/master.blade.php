6116c5aaafc30fb01ffc21023709fc95601a9098
Diza Jean <dizajpm24@gmail.com>
2017-09-12 12:12:03 +0800

    ..

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -55,7 +55,7 @@
     
       <ul class="social-links">
         <li><a href="#link"><i class="fa fa-twitter fa-fw"></i></a></li>
-        <li><a href="#link"><i class="fa fa-facebook fa-fw"></i></a></li>
+        <li><a href="www.facebook.com"><i class="fa fa-facebook fa-fw"></i></a></li>
         <li><a href="#link"><i class="fa fa-google-plus fa-fw"></i></a></li>
         <li><a href="#link"><i class="fa fa-dribbble fa-fw"></i></a></li>
         <li><a href="#link"><i class="fa fa-linkedin fa-fw"></i></a></li>
