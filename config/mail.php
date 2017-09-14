35aa2e9ae7fba6845eb53aa8b3c5e9ff79e28f55
Diza Jean <dizajpm24@gmail.com>
2017-09-14 20:20:57 +0800

    ...

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'reytabs1993@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'Kevin Rey'),
+        'address' => env('MAIL_FROM_ADDRESS', 'cebupound@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'CebuPound'),
     ],
 
     /*
