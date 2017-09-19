f3d735a16d4a9c8b701058f6be926214c1c489ba
Kevin Rey Tabada <reytabs1993@gmail.com>
2017-09-19 10:51:12 +0800

    Convert to use Heroku PostgreSQL database

--- config/database.php
+++ config/database.php
@@ -31,7 +31,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
     
     /*
     |--------------------------------------------------------------------------
