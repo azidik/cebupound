9d6de65ee0635882d032c6a844d9f3a380feb5d0
Kevin Rey Tabada <reytabs1993@gmail.com>
2017-09-20 08:53:25 +0800

    ..

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+// $host = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $database = substr($url["path"], 1);
 return [
 
     /*
@@ -57,7 +57,7 @@ return [
             'prefix' => '',
         ],
 
-        'mysql1' => [
+        'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
@@ -71,16 +71,16 @@ return [
             'engine' => null,
         ],
 
-        'mysql' => array(
-            'driver'    => 'mysql',
-            'host'      => $host,
-            'database'  => $database,
-            'username'  => $username,
-            'password'  => $password,
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        )
+        // 'mysql1' => array(
+        //     'driver'    => 'mysql',
+        //     'host'      => $host,
+        //     'database'  => $database,
+        //     'username'  => $username,
+        //     'password'  => $password,
+        //     'charset'   => 'utf8',
+        //     'collation' => 'utf8_unicode_ci',
+        //     'prefix'    => '',
+        // )
     ],
 
     /*
