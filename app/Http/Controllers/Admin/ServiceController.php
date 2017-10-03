5d7d78dec3fb848adea4535d5709961e89e5ea15
Kevin Rey Tabada <reytabs1993@gmail.com>
2017-10-03 23:12:04 +0800

    ...

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -31,35 +31,9 @@ class ServiceController extends Controller
         $serviceSchedule->status = 'Confirmed';
         $serviceSchedule->save();
 
-        return $serviceSchedule;
-
-        // $notification = new Notification;
-        // $notification->user_id = $serviceSchedule['pet']['user']['id'];
-        // $notification->mesage = 'Your pet has been scheduled for service on'. date('F j, Y', strtotime($serviceSchedule['schedule']));
-        // $notification->is_read = 0;
-        // $notification->save();
-
-        // // if($notification) {
-        //     $optionBuilder = new OptionsBuilder();
-        //     $optionBuilder->setTimeToLive(60*20);
-            
-        //     $notificationBuilder = new PayloadNotificationBuilder('Service Scheduled');
-        //     $notificationBuilder->setBody("Hi! Your pet scheduled for services on". date('F j, Y', strtotime($serviceSchedule['schedule'])))
-        //                         ->setSound('default');
-                                
-        //     $dataBuilder = new PayloadDataBuilder();
-        //     $dataBuilder->addData(['a_data' => 'my_data']);
-            
-        //     $option = $optionBuilder->build();
-        //     $notification = $notificationBuilder->build();
-        //     $data = $dataBuilder->build();
-            
-        //     $token = 'f3NSEhMsdUE:APA91bFnBij9TZUKmCJms0gP904Oj1Dz2YmumMv4TghfiTi2tPxbeuuJUkteyU0o6QljtD5M2GvzO-ZTv-2gq-lPs2nH080quBbGGOeeQigPvu39OW-EQB80GzvpWT7OhaY5idYXBiRb';
-            
-        //     $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
-        // // }
-
         if($serviceSchedule){    
+            if($serviceSchedule->pet->user->device_token != NULL || $serviceSchedule->pet->user->device_token != "" || $serviceSchedule->pet->user->device_token != 'undefined')
+                $this->sendNotification($serviceSchedule);
             $response = [
                 'status' => 1
             ];
@@ -74,8 +48,8 @@ class ServiceController extends Controller
     public function sendNotification($serviceSchedule)
     {   
         $notification = new Notification;
-        $notification->user_id = $serviceSchedule['pet']['user']['id'];
-        $notification->mesage = 'Your pet has been scheduled for service on'. date('F j, Y', strtotime($serviceSchedule['schedule']));
+        $notification->user_id = $serviceSchedule->pet->user->id;
+        $notification->mesage = 'Your pet has been scheduled for service on'. date('F j, Y', strtotime($serviceSchedule->schedule));
         $notification->is_read = 0;
         $notification->save();
 
@@ -84,7 +58,7 @@ class ServiceController extends Controller
             $optionBuilder->setTimeToLive(60*20);
             
             $notificationBuilder = new PayloadNotificationBuilder('Service Scheduled');
-            $notificationBuilder->setBody("Hi! Your pet scheduled for services on". date('F j, Y', strtotime($serviceSchedule['schedule'])))
+            $notificationBuilder->setBody("Hi! Your pet scheduled for services on". date('F j, Y', strtotime($serviceSchedule->schedule)))
                                 ->setSound('default');
                                 
             $dataBuilder = new PayloadDataBuilder();
@@ -94,7 +68,7 @@ class ServiceController extends Controller
             $notification = $notificationBuilder->build();
             $data = $dataBuilder->build();
             
-            $token = $serviceSchedule['pet']['user']['device_token'];
+            $token = $serviceSchedule->pet->user->device_token;
             
             $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
         }
