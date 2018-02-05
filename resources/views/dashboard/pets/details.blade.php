22a7ba1030b0599af6b45b93b38d5dfff8990b5d
Kevin Rey Tabada <reytabs1993@gmail.com>
2018-02-05 23:10:27 +0800

    ...

--- resources/views/dashboard/pets/details.blade.php
+++ resources/views/dashboard/pets/details.blade.php
@@ -35,53 +35,53 @@
             
             <!-- /.box-header -->
             <!-- form start -->
-            <form role="form" action="{{ url('dashboard/pets/update/'. $pet->id) }}" method="post">
+            <form role="form" action="{{ url('dashboard/pets/update/'. $pet['id']) }}" method="post">
                 {{ csrf_field() }}
                 <div class="box-body ">
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
                             <label for="exampleInputEmail1">Name</label>
-                            <input type="text" name="name" class="form-control" value="{{ $pet->name }}">
+                            <input type="text" name="name" class="form-control" value="{{ $pet['name'] }}">
                             </div>
                             <div class="form-group">
                             <label for="exampleInputPassword1">Birth Date</label>
-                            <input type="date" name="birth_date" id="birth_date" class="form-control" value="{{ $pet->birth_date }}" readonly>
+                            <input type="date" name="birth_date" id="birth_date" class="form-control" value="{{ $pet['birth_date'] }}" readonly>
                             </div>
                             <div class="form-group">
                             <label for="exampleInputPassword1">Age</label>
-                            <input type="text" name="age" class="form-control" value="{{ $pet->age }}" readonly>
+                            <input type="text" name="age" class="form-control" value="{{ $pet['age'] }}" readonly>
                             </div>
                             <div class="form-group">
                             <label>Gender</label>
                             <select class="form-control" name="gender" disabled>
-                                <option value="Male" {{$pet->gender == 'Male'}} ?? 'selected' : ''>Male</option>
-                                <option value="Female" {{$pet->gender == 'Female'}} ?? 'selected' : ''>Female</option>
+                                <option value="Male" {{$pet['gender'] == 'Male'}} ?? 'selected' : ''>Male</option>
+                                <option value="Female" {{$pet['gender'] == 'Female'}} ?? 'selected' : ''>Female</option>
                             </select>
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputFile">
-                                    <img src="{{ asset('/images/'. $pet->image)}}" width="200">
+                                    <img src="{{ asset('/images/'. $pet['image'])}}" width="200">
                                 </label>
-                                <input type="file" name="image" value="{{ $pet->image}}">
+                                <input type="file" name="image" value="{{ $pet['image']}}">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                             <label >Breed</label>
-                            <input type="text" name="breed" class="form-control" value="{{ $pet->breed->name }}" readonly>
+                            <input type="text" name="breed" class="form-control" value="{{ $pet['breed']['name'] }}" readonly>
                             </div>
                             <div class="form-group">
                             <label >Color</label>
-                            <input type="text" name="color" class="form-control" value="{{ $pet->color }}" readonly>
+                            <input type="text" name="color" class="form-control" value="{{ $pet['color'] }}" readonly>
                             </div>
                             <div class="form-group">
                             <label>Type</label>
-                            <input type="hidden" name="gender" value="{{ $pet->gender }}">
-                            <input type="hidden" name="pet_type_id" value="{{ $pet->pet_type_id }}">
+                            <input type="hidden" name="gender" value="{{ $pet['gender'] }}">
+                            <input type="hidden" name="pet_type_id" value="{{ $pet['pet_type_id'] }}">
                             <select class="form-control" name="pet_type_id" disabled>
                                 @foreach($types as $type)
-                                <option value="{{$type->id}}" {{$pet->pet_type_id == $type->id}} ?? 'selected' : ''>{{$type->name}}</option>
+                                <option value="{{$type['id']}}" {{$pet['pet_type_id'] == $type['id']}} ?? 'selected' : ''>{{$type['name']}}</option>
                                 @endforeach
                             </select>
                             </div>
