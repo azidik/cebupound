b0ddf232fc08c3c5799df8c7760416dba6c602d4
jenrecalp <leafame16@gmail.com>
2018-02-09 07:38:53 +0800

    numeric@register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -52,7 +52,7 @@
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="text" class="form-control" name="contact_no" placeholder="Contact No.">
+        <input type="number" class="form-control" name="contact_no" placeholder="Contact No.">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
